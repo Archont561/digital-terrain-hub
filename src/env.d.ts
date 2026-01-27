9a6f85987399db7c00075e544ecf2579b5dfc164
Eryk Srebrny <esrebrny@onet.eu>
2026-01-27 22:30:48 +0100

    refactor: change env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,37 +1,38 @@
 /// <reference types="astro/client" />
 /// <reference path="../.astro/types.d.ts" />
 
+export {};
+
 declare global {
   interface Window {
+    Alpine: import("alpinejs").Alpine;
   }
-}
 
-namespace App {
-  interface Locals {
-    currentUserId: string | null;
+  namespace App {
+    interface Locals {
+      currentUserId: string | null;
+    }
   }
-}
-
-declare namespace NodeJS {
-  interface ProcessEnv {
-    NODE_ENV: 'dev' | 'prod' | 'test';
-    TEST_ASTRO_BASE_URL: string;
-    TEST_CLERK_AUTH_PLAYWRIGHT_STORAGE_FILE: string;
-    TEST_AUTHENTICATED_CLERK_USER_ID: string;
-    TEST_NINJAODM_SECRET_KEY: string;
-    TEST_NINJAODM_BASE_MOCK_SERVER_URL: string;
-    NINJAODM_SECRET_KEY: string;
-    NINJAODM_API_KEY: string;
-    NINJAODM_BASE_URL: string;
-    PATH_TO_NINJAODM_OPENAPI_SPEC: string;
+  
+  declare namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: 'dev' | 'prod' | 'test';
+      ASTRO_BASE_URL: string;
+      TEST_CLERK_AUTH_PLAYWRIGHT_STORAGE_FILE: string;
+      TEST_AUTHENTICATED_CLERK_USER_ID: string;
+      NINJAODM_SECRET_KEY: string;
+      NINJAODM_API_KEY: string;
+      NINJAODM_BASE_URL: string;
+    }
   }
-}
 
 
-// https://docs.astro.build/en/guides/environment-variables/#intellisense-for-typescript
-interface ImportMetaEnv {
-}
+  // https://docs.astro.build/en/guides/environment-variables/#intellisense-for-typescript
+  interface ImportMetaEnv {
+  }
 
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+
 }

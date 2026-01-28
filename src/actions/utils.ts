68fa2396c186256785d0019e0b3cfdcc09d6b025
Eryk Srebrny <esrebrny@onet.eu>
2026-01-28 16:30:28 +0100

    feat(actions): create astro actions utility constants

---
+++ src/actions/utils.ts
@@ -0,0 +1,11 @@
+import { createServiceApiClient, HMACAuthorization } from "@/lib";
+
+export const hmacAuth = HMACAuthorization.create({
+  apiKey: process.env.NINJAODM_API_KEY!,
+  apiSecret: process.env.NINJAODM_SECRET_KEY!,
+});
+
+export const serviceApiClient = createServiceApiClient({
+  baseUrl: process.env.NINJAODM_BASE_URL!,
+  hmacAuth,
+});

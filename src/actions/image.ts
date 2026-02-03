f7b943df776dd780f965281b1bac9039edbbfff6
Archont561 <142521648+Archont561@users.noreply.github.com>
2026-02-03 22:27:38 +0000

    feat(actions): implement Image astro actions

---
+++ src/actions/image.ts
@@ -0,0 +1,41 @@
+import { defineAction } from "astro:actions";
+import { z } from "astro/zod";
+import { serviceApiClient } from "./utils";
+
+export const actions = {
+  getImage: defineAction({
+    input: z.object({
+      uuid: z.string().uuid(),
+    }),
+    handler: async ({ uuid }) => {
+      return await serviceApiClient.getImage({
+        params: { uuid },
+      });
+    },
+  }),
+  deleteImage: defineAction({
+    input: z.object({
+      uuid: z.string().uuid(),
+    }),
+    handler: async ({ uuid }) => {
+      return await serviceApiClient.deleteImage(undefined, {
+        params: { uuid },
+      });
+    },
+  }),
+
+  listImages: defineAction({
+    input: z
+      .object({
+        name: z.string().optional(),
+        is_thumbnail: z.boolean().optional(),
+        workspace_uuid: z.string().uuid().optional(),
+        created_after: z.string().nullable().optional(),
+        created_before: z.string().nullable().optional(),
+      })
+      .optional(),
+    handler: async (queries) => {
+      return await serviceApiClient.listImagesInternal({ queries });
+    },
+  }),
+};
--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,10 +1,11 @@
+import { actions as imageActions } from "./image";
 import { actions as resultActions } from "./result";
 import { actions as taskActions } from "./task";
 import { actions as workspaceActions } from "./workspace";
-import { actions as taskActions } from "./task";
 
 export const server = {
   ...workspaceActions,
   ...resultActions,
   ...taskActions,
+  ...imageActions,
 };

78549af00411436b495365f7d102f08b86d643e8
Archont561 <142521648+Archont561@users.noreply.github.com>
2026-02-03 17:20:08 +0000

    chore: load env vars when starting astro server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
+import path from "node:path";
 import node from "@astrojs/node";
 import clerk from "@clerk/astro";
 import runtimeLogger from "@inox-tools/runtime-logger";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import dotenv from "dotenv";
+
+const envFile = path.resolve(import.meta.dirname, ".env.local");
+dotenv.config({ path: envFile });
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    clerk(),
-    runtimeLogger(),
-  ],
+  integrations: [clerk(), runtimeLogger()],
   vite: {
     plugins: [tailwindcss()],
   },

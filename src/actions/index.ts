e65a77b4543919d563ee1e24dc0cb24dd3455ed0
Archont561 <142521648+Archont561@users.noreply.github.com>
2026-02-03 22:16:02 +0000

    feat(actions): implement Task astro actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,7 +1,10 @@
 import { actions as resultActions } from "./result";
+import { actions as taskActions } from "./task";
 import { actions as workspaceActions } from "./workspace";
+import { actions as taskActions } from "./task";
 
 export const server = {
   ...workspaceActions,
   ...resultActions,
+  ...taskActions,
 };
---
+++ src/actions/task.ts
@@ -0,0 +1,66 @@
+import { defineAction } from "astro:actions";
+import { z } from "astro/zod";
+import { schemas } from "@/lib";
+import { serviceApiClient } from "./utils";
+
+export const actions = {
+  createTask: defineAction({
+    input: z.object({
+      workspace_uuid: z.string().uuid(),
+      name: z.string(),
+      quality: schemas.ODMQualityOption.optional(),
+    }),
+    handler: async ({ workspace_uuid, name, quality }) => {
+      return await serviceApiClient.createTask({
+        workspace_uuid,
+        name,
+        quality,
+      });
+    },
+  }),
+  getTask: defineAction({
+    input: z.object({
+      uuid: z.string().uuid(),
+    }),
+    handler: async ({ uuid }) => {
+      return await serviceApiClient.getTask({
+        params: { uuid },
+      });
+    },
+  }),
+  callTaskAction: defineAction({
+    input: z.object({
+      uuid: z.string().uuid(),
+      action: z.enum(["pause", "resume", "cancel"]),
+    }),
+    handler: async ({ uuid, action }) => {
+      return await serviceApiClient.callTaskAction(undefined, {
+        params: { uuid, action },
+      });
+    },
+  }),
+  deleteTask: defineAction({
+    input: z.object({
+      uuid: z.string().uuid(),
+    }),
+    handler: async ({ uuid }) => {
+      return await serviceApiClient.deleteTask(undefined, {
+        params: { uuid },
+      });
+    },
+  }),
+  listTask: defineAction({
+    input: z
+      .object({
+        step: schemas.ODMProcessingStage.optional(),
+        status: schemas.ODMTaskStatus.optional(),
+        workspace_uuid: z.string().uuid().optional(),
+        created_after: z.string().nullable().optional(),
+        created_before: z.string().nullable().optional(),
+      })
+      .optional(),
+    handler: async (queries) => {
+      return await serviceApiClient.listTasksInternal({ queries });
+    },
+  }),
+} as const;

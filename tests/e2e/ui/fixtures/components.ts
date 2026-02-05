1eef23ac78bed009040744a1e497a63ee9c8368c
Archont561 <142521648+Archont561@users.noreply.github.com>
2026-02-05 23:00:45 +0000

    refactor(fixtures): use custom merging for ComponentPageNavigator fixture for e2e tests

--- tests/e2e/ui/fixtures/components.ts
+++ tests/e2e/ui/fixtures/components.ts
@@ -11,7 +11,7 @@ export abstract class ComponentPageNavigator<TProps> {
   protected abstract readonly rootSelector: string;
   protected abstract readonly selectors: Record<string, string>;
 
-  constructor(protected readonly page: Page) {}
+  constructor(protected readonly page: Page) { }
 
   get root(): Locator {
     return this.page.locator(this.rootSelector).first();
@@ -35,6 +35,11 @@ export abstract class ComponentPageNavigator<TProps> {
 
   getProps(propsOverride?: DeepPartial<TProps>): TProps {
     if (!propsOverride) return this.defaultProps;
-    return _.merge({}, this.defaultProps, propsOverride);
+
+    return _.mergeWith({}, this.defaultProps, propsOverride, (objValue, srcValue) => {
+      if (_.isArray(objValue) && _.isArray(srcValue)) {
+        return srcValue; // fully override arrays
+      }
+    });
   }
 }

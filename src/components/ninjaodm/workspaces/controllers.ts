a7bbe8d8cbfe8c5055d2544dc00ac230257f76d7
Archont561 <142521648+Archont561@users.noreply.github.com>
2026-02-08 15:42:49 +0000

    feat(components): update WorkspaceManager to allow creating tasks

--- src/components/ninjaodm/workspaces/controllers.ts
+++ src/components/ninjaodm/workspaces/controllers.ts
@@ -1,6 +1,7 @@
 import { actions } from "astro:actions";
 import { AlpineController, runAction } from "@/lib/client";
 import { Workspace, type WorkspaceProps } from "./models";
+import { toast } from "@/components/starwind/toast";
 
 export type WorkspaceManagerProps = {
   workspaces?: WorkspaceProps[];
@@ -13,11 +14,15 @@ export class WorkspaceManager extends AlpineController<
   {
     workspaceImageUploadDialogTrigger: HTMLButtonElement;
     tusImageUploader: HTMLDivElement;
+    workspaceTaskCreationDialogTrigger: HTMLButtonElement;
+    workspaceTaskCreationDialogCloseBtn: HTMLButtonElement;
+    taskCreationForm: HTMLFormElement;
   }
 > {
   private workspaces: Workspace[];
   private baseEditUrl = "";
   private workspaceImageUploadUrlTemplate = "";
+  private currentWorkspace: Workspace | null = null;
 
   constructor({ workspaces = [], baseEditUrl, workspaceImageUploadUrlTemplate }: WorkspaceManagerProps) {
     super();
@@ -69,6 +74,52 @@ export class WorkspaceManager extends AlpineController<
 
   handleWorkspaceImagesUpload(workspace: Workspace) {
     this.ctx.$refs.workspaceImageUploadDialogTrigger.click();
-    this.ctx.$refs.tusImageUploader.dataset.uploadUrl = this.workspaceImageUploadUrlTemplate.replace("uuid", workspace.uuid);    
+    this.ctx.$refs.tusImageUploader.dataset.uploadUrl = this.workspaceImageUploadUrlTemplate.replace("uuid", workspace.uuid);
+  }
+
+  handleWorkspaceTaskCreation(workspace: Workspace) {
+    this.ctx.$refs.taskCreationForm.reset();
+    this.ctx.$refs.workspaceTaskCreationDialogTrigger.click();
+    this.currentWorkspace = workspace;
+  }
+
+  validateTaskForm() {
+    const formData = new FormData(this.ctx.$refs.taskCreationForm);
+
+    const name = (formData.get("name") as string | null)?.trim();
+    const quality = formData.get("quality");
+
+    if (!name) {
+      toast.error("Task name cannot be empty", { duration: 2000 });
+      return false;
+    }
+
+    if (!quality) {
+      toast.error("Define task results quality", { duration: 2000 });
+      return false;
+    }
+
+    return { name, quality };
+  }
+
+  async handleTaskCreationSubmit() {
+    if (!this.currentWorkspace) return;
+    const validated = this.validateTaskForm();
+    if (!validated) return;
+
+    await runAction(
+      actions.createTask({
+        workspace_uuid: this.currentWorkspace.uuid,
+        name: validated.name as any,
+        quality: validated.quality as any,
+      }),
+      {
+        successMessage: "Task created successfully",
+        onSuccess: () => {
+          this.ctx.$refs.workspaceTaskCreationDialogCloseBtn.click();
+          this.currentWorkspace = null;
+        }
+      },
+    );
   }
 }
